b1a8cdc939fb0537eff50e2192a63eb7710084ef
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2022-02-07 18:56:13 +0100

    Add files via upload

--- calc/main.c
+++ calc/main.c
@@ -2,18 +2,23 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
+
+///a basic C calculator
+///by shadowdev
 
 int main() {
-    SetConsoleTitle("C calc");
+    SetConsoleTitle("C calculator");
     int value1;
     int value2;
     int output;
-    int checkforexit[10];
     printf("how to use:\n");
     printf("add two values must be numbers to get output\n");
+    printf("created by shadowdev on github: https://github.com/shadow9owo\n");
     getchar();
     while (TRUE)
     {
+       system("cls||clear");
        printf("enter first value: \n");
        scanf("%d",&value1);
        printf("enter second value: \n");
@@ -21,5 +26,6 @@ int main() {
        output = value1 + value2;
        printf("output:\n");
        printf("%d \n",output);
+       Sleep(2000);
     }
 }
